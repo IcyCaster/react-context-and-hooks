3c442dc6ed790333aafa183aa206a241d3a8978f
IcyCaster <chester.booker.nz@gmail.com>
2019-12-26 19:19:36 +1300

    Lesson 2: Add context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import Navbar from "./components/Navbar";
 import Booklist from "./components/Booklist";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <ThemeContextProvider>
                 <Navbar/>
                 <Booklist/>
+                </ThemeContextProvider>
             </div>
         );
     }
