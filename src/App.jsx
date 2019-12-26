578e62e1930fe4b95865006afb2b8a284551ad30
IcyCaster <chester.booker.nz@gmail.com>
2019-12-26 20:20:43 +1300

    Lesson 3: Adding context consumers

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import React, {Component} from 'react';
 import Navbar from "./components/Navbar";
 import Booklist from "./components/Booklist";
 import ThemeContextProvider from "./contexts/ThemeContext";
+import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <ThemeContextProvider>
-                <Navbar/>
-                <Booklist/>
-                </ThemeContextProvider>
+                <AuthContextProvider>
+                    <ThemeContextProvider>
+                        <Navbar/>
+                        <Booklist/>
+                        <ThemeToggle/>
+                    </ThemeContextProvider>
+                </AuthContextProvider>
             </div>
         );
     }
--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
+import {ThemeContext} from "../contexts/ThemeContext";
 
 class Booklist extends Component {
     render() {
         return (
-            <div className='book-list'>
-                <ul>
-                    <li>Fire book</li>
-                    <li>Water book</li>
-                    <li>Earth book</li>
-                    <li>Air book</li>
-                </ul>
-            </div>
+            <ThemeContext.Consumer>{(context) => {
+                const {isLightTheme, light, dark } = context;
+                const theme = isLightTheme ? light : dark;
+
+                return <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
+                    <ul>
+                        <li style={{background: theme.ui}}>Fire book</li>
+                        <li style={{background: theme.ui}}>Water book</li>
+                        <li style={{background: theme.ui}}>Earth book</li>
+                        <li style={{background: theme.ui}}>Air book</li>
+                    </ul>
+                </div>
+            }}
+            </ThemeContext.Consumer>
         );
     }
 }
